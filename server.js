54bc7947422e5e4d94e779fb521417a80a09fa3e
pmwita <pmwita2017@gmail.com>
2024-09-19 16:47:34 +0300

    Initial commit

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema'); // Ensure this points to your GraphQL schema
+const schema = require('./schema');
 
 const app = express();
 
-// Enable CORS for all origins
-app.use(cors());
+app.use(cors()); // Enable CORS for all origins
 app.use(express.json()); // Parse JSON requests
 
-// Use environment variable for GraphQL server port
-const PORT = process.env.PORT || 4000;
-
-// Set up the GraphQL endpoint
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}..`);
+app.listen(4000, () => {
+  console.log('Server is running on port 4000..');
 });
